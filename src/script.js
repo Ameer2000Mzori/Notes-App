544553aa775498ca79294ae63b69ef4999eee4fc
Ameer <ameerapex496@gmail.com>
2023-12-07 16:57:22 +0100

    added comment + selected elements + added some logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,35 @@
-console.log("hallo world");
+// selecting elements
+var addNoteBtn = document.getElementsByClassName("add-Note")[0];
+// global varibales
+var notesObject = [
+    {
+        text: "text one",
+    },
+    {
+        text: "text two",
+    },
+    {
+        text: "text three",
+    },
+];
+// functions
+var createNote = function () {
+    notesObject.forEach(function (el) {
+        var noteCard = document.createElement("div");
+        noteCard.classList.add("note-Card");
+    });
+};
+// event linsters
+addNoteBtn.addEventListener("click", createNote);
+// our note html tree :
+// <div class="note-Card">
+// <div class="upper-Note-Side">
+//   <button class="edit-Btn">
+//     <i class="fa-solid fa-pen-to-square"></i>
+//   </button>
+//   <button class="delete-Btn">
+//     <i class="fa-solid fa-trash"></i>
+//   </button>
+// </div>
+// <textarea class="note-Text-Area"></textarea>
+// </div>
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,40 @@
-console.log("hallo world");
+// selecting elements
+const addNoteBtn = document.getElementsByClassName("add-Note")[0];
+
+// global varibales
+const notesObject = [
+  {
+    text: "text one",
+  },
+  {
+    text: "text two",
+  },
+  {
+    text: "text three",
+  },
+];
+
+// functions
+const createNote = () => {
+  notesObject.forEach((el) => {
+    const noteCard: Element = document.createElement("div");
+    noteCard.classList.add("note-Card");
+  });
+};
+
+// event linsters
+addNoteBtn.addEventListener("click", createNote);
+
+// our note html tree :
+
+// <div class="note-Card">
+// <div class="upper-Note-Side">
+//   <button class="edit-Btn">
+//     <i class="fa-solid fa-pen-to-square"></i>
+//   </button>
+//   <button class="delete-Btn">
+//     <i class="fa-solid fa-trash"></i>
+//   </button>
+// </div>
+// <textarea class="note-Text-Area"></textarea>
+// </div>
