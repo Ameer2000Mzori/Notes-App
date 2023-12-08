0d5c72b0ed20d39ebdbd4d95f6d96a94d946c5d1
Ameer <ameerapex496@gmail.com>
2023-12-08 06:32:21 +0100

    fixed linters, added dynamic html tree to the project

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 // selecting elements
 var addNoteBtn = document.getElementsByClassName("add-Note")[0];
+var notesWrap = document.getElementsByClassName("notes-Wrap")[0];
 // global varibales
 var notesObject = [
     {
@@ -17,19 +18,27 @@ var createNote = function () {
     notesObject.forEach(function (el) {
         var noteCard = document.createElement("div");
         noteCard.classList.add("note-Card");
+        var upperNoteSide = document.createElement("div");
+        upperNoteSide.classList.add("upper-Note-Side");
+        noteCard.appendChild(upperNoteSide);
+        var editBtn = document.createElement("button");
+        editBtn.classList = "edit-Btn";
+        upperNoteSide.appendChild(editBtn);
+        var iEl = document.createElement("i");
+        iEl.classList.add("fa", "fa-solid", "fa-pen-to-square");
+        editBtn.appendChild(iEl);
+        var deleteBtn = document.createElement("button");
+        deleteBtn.classList = "delete-Btn";
+        upperNoteSide.appendChild(deleteBtn);
+        var deleteIEl = document.createElement("i");
+        deleteIEl.classList.add("fa-solid", "fa-trash");
+        deleteBtn.appendChild(deleteIEl);
+        var noteTextArea = document.createElement("textarea");
+        noteTextArea.classList.add("note-Text-Area");
+        noteTextArea.textContent = "".concat(el.text);
+        noteCard.appendChild(noteTextArea);
+        notesWrap.appendChild(noteCard);
     });
 };
 // event linsters
 addNoteBtn.addEventListener("click", createNote);
-// our note html tree :
-// <div class="note-Card">
-// <div class="upper-Note-Side">
-//   <button class="edit-Btn">
-//     <i class="fa-solid fa-pen-to-square"></i>
-//   </button>
-//   <button class="delete-Btn">
-//     <i class="fa-solid fa-trash"></i>
-//   </button>
-// </div>
-// <textarea class="note-Text-Area"></textarea>
-// </div>
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
 // selecting elements
 const addNoteBtn = document.getElementsByClassName("add-Note")[0];
+const notesWrap = document.getElementsByClassName("notes-Wrap")[0];
 
 // global varibales
 const notesObject = [
@@ -19,22 +20,35 @@ const createNote = () => {
   notesObject.forEach((el) => {
     const noteCard: Element = document.createElement("div");
     noteCard.classList.add("note-Card");
+
+    const upperNoteSide: Element = document.createElement("div");
+    upperNoteSide.classList.add("upper-Note-Side");
+    noteCard.appendChild(upperNoteSide);
+
+    const editBtn: any = document.createElement("button");
+    editBtn.classList = "edit-Btn";
+    upperNoteSide.appendChild(editBtn);
+
+    const iEl = document.createElement("i");
+    iEl.classList.add("fa", "fa-solid", "fa-pen-to-square");
+    editBtn.appendChild(iEl);
+
+    const deleteBtn: any = document.createElement("button");
+    deleteBtn.classList = "delete-Btn";
+    upperNoteSide.appendChild(deleteBtn);
+
+    const deleteIEl = document.createElement("i");
+    deleteIEl.classList.add("fa-solid", "fa-trash");
+
+    deleteBtn.appendChild(deleteIEl);
+
+    const noteTextArea = document.createElement("textarea");
+    noteTextArea.classList.add("note-Text-Area");
+    noteTextArea.textContent = `${el.text}`;
+    noteCard.appendChild(noteTextArea);
+    notesWrap.appendChild(noteCard);
   });
 };
 
 // event linsters
 addNoteBtn.addEventListener("click", createNote);
-
-// our note html tree :
-
-// <div class="note-Card">
-// <div class="upper-Note-Side">
-//   <button class="edit-Btn">
-//     <i class="fa-solid fa-pen-to-square"></i>
-//   </button>
-//   <button class="delete-Btn">
-//     <i class="fa-solid fa-trash"></i>
-//   </button>
-// </div>
-// <textarea class="note-Text-Area"></textarea>
-// </div>
