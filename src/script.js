6c536775788b76ff92ce19e2286ecbfe83b8d76d
Ameer <ameerapex496@gmail.com>
2023-12-08 09:09:30 +0100

    added new functions / edit / added new object

--- src/script.js
+++ src/script.js
@@ -13,8 +13,13 @@ var notesObject = [
         text: "text three",
     },
 ];
+var newCreatedNote = [
+    {
+        text: "ADD YOUR TEXT HERE!",
+    },
+];
 // functions
-var createNote = function () {
+var listNotes = function () {
     notesObject.forEach(function (el) {
         var noteCard = document.createElement("div");
         noteCard.classList.add("note-Card");
@@ -25,7 +30,7 @@ var createNote = function () {
         editBtn.classList = "edit-Btn";
         upperNoteSide.appendChild(editBtn);
         var iEl = document.createElement("i");
-        iEl.classList.add("fa", "fa-solid", "fa-pen-to-square");
+        iEl.classList.add("fa-solid", "fa-pen-to-square");
         editBtn.appendChild(iEl);
         var deleteBtn = document.createElement("button");
         deleteBtn.classList = "delete-Btn";
@@ -38,7 +43,7 @@ var createNote = function () {
         noteTextArea.setAttribute("readonly", "");
         noteTextArea.textContent = "".concat(el.text);
         noteCard.appendChild(noteTextArea);
-        notesWrap.appendChild(noteCard);
+        notesWrap.prepend(noteCard);
         // our eventlinsters within the dynamic dom creating
         deleteBtn.addEventListener("click", function (e) {
             var thisTextNote = e.target.parentElement.parentElement.nextElementSibling.textContent.trim();
@@ -52,6 +57,23 @@ var createNote = function () {
             }
             console.log("tested");
         });
+        // our edit text eventlinster
+        editBtn.addEventListener("click", function (e) {
+            var editNote = e.target.parentElement.parentElement.nextElementSibling;
+            if (editNote.readOnly) {
+                console.log("read only removed");
+                editNote.readOnly = false;
+                iEl.classList.replace("fa-pen-to-square", "fa-check");
+                iEl.style.color = "red";
+            }
+            else if (!editNote.readOnly) {
+                console.log("read only added");
+                editNote.readOnly = true;
+                iEl.classList.replace("fa-check", "fa-pen-to-square");
+                iEl.style.removeProperty("color");
+            }
+            editFunction(editNote);
+        });
     });
 };
 // // delete notes from object dynamclly :
@@ -60,5 +82,100 @@ var deleteNote = function (thisTextNote) {
     notesObject = notesObject.filter(function (note) { return note.text.trim().toLowerCase() !== thisTextNote.toLowerCase(); });
     console.log(notesObject);
 };
+// edit function
+var editFunction = function (editNote) {
+    console.log(editNote.value);
+    editNote.addEventListener("change", function () {
+        var newText = editNote.value;
+        console.log(newText);
+        if (!notesObject.filter(function (el) { return el.text === newText; }).length) {
+            var newNote = {
+                text: newText,
+            };
+            notesObject.push(newNote);
+            console.log("object after edit", notesObject);
+            notesWrap.innerHTML = "";
+            listNotes();
+        }
+        else {
+            console.log("nothing happend");
+        }
+    });
+};
+// // createNote dynamclly
+// const createNote = () => {
+//   newCreatedNote.forEach((el) => {
+//     const noteCard: Element = document.createElement("div");
+//     noteCard.classList.add("note-Card");
+//     const upperNoteSide: Element = document.createElement("div");
+//     upperNoteSide.classList.add("upper-Note-Side");
+//     noteCard.appendChild(upperNoteSide);
+//     const editBtn: any = document.createElement("button");
+//     editBtn.classList = "edit-Btn";
+//     upperNoteSide.appendChild(editBtn);
+//     const iEl = document.createElement("i");
+//     iEl.classList.add("fa-solid", "fa-pen-to-square");
+//     editBtn.appendChild(iEl);
+//     const deleteBtn: any = document.createElement("button");
+//     deleteBtn.classList = "delete-Btn";
+//     upperNoteSide.appendChild(deleteBtn);
+//     const deleteIEl = document.createElement("i");
+//     deleteIEl.classList.add("fa-solid", "fa-trash");
+//     deleteBtn.appendChild(deleteIEl);
+//     const noteTextArea = document.createElement("textarea");
+//     noteTextArea.classList.add("note-Text-Area");
+//     noteTextArea.setAttribute("readonly", "");
+//     noteTextArea.textContent = `${el.text}`;
+//     noteCard.appendChild(noteTextArea);
+//     notesWrap.prepend(noteCard);
+//     // our eventlinsters within the dynamic dom creating
+//     deleteBtn.addEventListener("click", (e) => {
+//       const thisTextNote: string =
+//         e.target.parentElement.parentElement.nextElementSibling.textContent.trim();
+//       if (thisTextNote) {
+//         deleteNote(thisTextNote);
+//         notesWrap.removeChild(noteCard);
+//         console.log("tested in if block");
+//       } else {
+//         console.log("nothing here");
+//       }
+//       console.log("tested");
+//     });
+//     // our edit text eventlinster
+//     editBtn.addEventListener("click", (e) => {
+//       console.log(`object before edit`, notesObject);
+//       let editNote = e.target.parentElement.parentElement.nextElementSibling;
+//       if (editNote.readOnly) {
+//         console.log("read only removed");
+//         editNote.readOnly = false;
+//         iEl.classList.replace("fa-pen-to-square", "fa-check");
+//         iEl.style.color = "red";
+//       } else if (!editNote.readOnly) {
+//         console.log("read only added");
+//         editNote.readOnly = true;
+//         iEl.classList.replace("fa-check", "fa-pen-to-square");
+//         iEl.style.removeProperty("color");
+//       }
+//       editNote.addEventListener("change", () => {
+//         const newText = (noteTextArea.textContent = noteTextArea.value);
+//         console.log(newText);
+//         if (!notesObject.filter((el) => el.text === newText).length) {
+//           let newNote = {
+//             text: newText,
+//           };
+//           notesObject.push(newNote);
+//           console.log((noteTextArea.textContent, noteTextArea.value));
+//           console.log(`object after edit`, notesObject);
+//           notesWrap.innerHTML = ``;
+//           listNotes();
+//         } else {
+//           console.log("nothing happend");
+//         }
+//       });
+//     });
+//   });
+// };
+// this is edit function
 // event linsters
-addNoteBtn.addEventListener("click", createNote);
+// addNoteBtn.addEventListener("click", createNote);
+listNotes();
